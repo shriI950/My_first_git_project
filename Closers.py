388ffbe63b87948e39c8457948591da7b86e4d1c
Shrirang Joshi <jshrirang8102001@gmail.com>
2022-07-29 19:36:38 +0530

    First Commit

---
+++ Closers.py
@@ -0,0 +1,12 @@
+
+
+def outerFunction(tag):
+    def innerFunction(content):
+        print(f'<{tag}>{content}</{tag}>')
+    return innerFunction
+
+header = outerFunction('h1')
+p = outerFunction('p')
+
+header("This is a h1 header.")
+p("This is a paragraph.")
